6adf07a6d1f983b3d9bde9a3407bfb1ffe5a69a5
johntilelli <johntilelli@gmail.com>
2017-05-17 10:13:43 -0700

    Update crime_svd_assn.r

--- crime_svd_assn.r
+++ crime_svd_assn.r
@@ -1,10 +1,6 @@
-
-
 # SVD on features then regression on those features for crime prediction
 
 library(logging)
-
-
 
 load_data = function(input_file, header_file){
   # Load data
@@ -26,8 +22,8 @@ load_data = function(input_file, header_file){
 # Unit test
 
 if(interactive()){
-  
-  setwd("/Users/Magic/Documents/UW/Methods/Week 6")
+  setwd("O:/R/Methods")
+
   
   # Setup logger
   logReset()
@@ -39,9 +35,9 @@ if(interactive()){
   # load data
   crime_df = load_data('communities.data', header_file = 'crime_headers.txt')
   
-  #loginfo('info')
-  #logdebug('debug')
-  #logwarn('warn')
+  loginfo('info')
+  logdebug('debug')
+  logwarn('warn')
   
   # Disregard 'state' and 'communityname'.
   crime_df$state = NULL
@@ -53,18 +49,13 @@ if(interactive()){
   
   # Regular logistic regression:
   regular_logistic = glm(ViolentCrimesPerPop ~ ., data = crime_df, family="binomial")
-  #Warning message:
-  #In eval(expr, envir, enclos) : non-integer #successes in a binomial glm!
   
-  # Predictions (above or below 0.5) don't make sense since we are predicting a probability (not binary event)
+  # Predictions (above or below 0.5) don't make sense since we are predicted a probability (not binary event)
   # So we use AIC as our measure
   regular_logistic_AIC = regular_logistic$aic
   
-  
-  
-  
   # Histogram of fitted values
-  hist(regular_logistic$fitted.values) #fitted values are probabilities
+  hist(regular_logistic$fitted.values)
   hist(crime_df$ViolentCrimesPerPop)
   
   # Bonus!!!! Can you think of a way to check that the hypothesis that the two distributions:
@@ -75,16 +66,7 @@ if(interactive()){
   #  (Not required for the homework... Just something to think about!)
   
   # Create SVD features and perform linear regression.
-  
-  # Full linear regression:
-  
-  # remove missing rows
-  crime_df = crime_df[complete.cases(crime_df),]
-  
-  log_price_all_model = lm(log(ViolentCrimesPerPop) ~ ., data = crime_df)
-  
-  summary(log_price_all_model)
-  
+  log_price_all_model = glm(ViolentCrimesPerPop ~ ., data = crime_df)
   # Step 1:  Create the data matrix:
   data_matrix = model.matrix(log(ViolentCrimesPerPop) ~. ,data = crime_df)  # modify this equation
   
@@ -94,29 +76,21 @@ if(interactive()){
   # Step 3: Look at magnitude of the variances explained (These are the eigenvalues!)
   plot(pc_data_matrix$sdev)
   
+  
   # Step 4: Perform linear regression on components:
-  pc_all_components = glm(crime_df$ViolentCrimesPerPop ~ pc_data_matrix$x[,1:102], family="binomial")
+  pc_all_components = glm(crime_df$ViolentCrimesPerPop ~ pc_data_matrix$x[,1:10], family="binomial")
   summary(pc_all_components)
   
   # Get aic
   pc_AIC = AIC(pc_all_components) # Slightly better...
-  
+  loginfo(pc_AIC)
   # What number of components minimizes the AIC?
   
   # Here, we use VERY similar code to the lines 181-192 from the "R_Examples_Lecture6.R"
   
-  # Report your findings.
-  
-  #fill in missing data
-  mean_norm_losses = mean(data$normalized_losses, na.rm=TRUE)
-  data$normalized_losses[is.na(data$normalized_losses)] = mean_norm_losses
-  
-  
-#error here... i think there are NAs somewhere... see 111
-  #should pc_all_components_temp be a glm or a lm? I think it's glm. 
   aic_by_num_pc = sapply(2:102, function(x){
-    formula_rhs_temp = paste(paste('pc_data_matrix$x[,',1:x,']'), collapse = ' + ')
-    formula_temp = paste('log(crime_df$ViolentCrimesPerPop) ~',formula_rhs_temp)
+    formula_rhs_temp = paste(paste0('pc_data_matrix$x[,',1:x,']'), collapse = ' + ')
+    formula_temp = paste('crime_df$ViolentCrimesPerPop ~',formula_rhs_temp)
     pc_all_components_temp = glm(eval(parse(text=formula_temp)))
     return(AIC(pc_all_components_temp))
   })
@@ -126,6 +100,8 @@ if(interactive()){
   # add a horizontal line of where the all variable AIC is at
   abline(h=AIC(log_price_all_model), lwd=2, col='red')
   which.min(aic_by_num_pc) # 59 principal components!
+  
+  # Report your findings.
   
 }
 
